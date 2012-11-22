05aaa07831b7a331dbb8b6b7b11d6b822542c51a
Rodrigo Gomes <rodrigomes@ubuntued.(none)>
2012-11-22 10:41:08 -0200

    My first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KokiriSpirit::Application.config.secret_token = 'dd412f28f674d53e1c6d4cbe592d37b11432c3fd29562803350a2a8776406c9b6238e332e839e3d7ccccb26f35a41b6525109adab72939414d9797a20ecc42e2'
