3ce822d59c78c58b3f2b3ff82bcc498525551f60
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-11-23 23:01:07 -0200

    Fix user selection in form

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -5,6 +5,9 @@ class Tree < ActiveRecord::Base
 
   acts_as_gmappable
 
+  validates_presence_of :user_id
+  validates_presence_of :nickname
+
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     self.adress
